e9a88764c8c0ea50233eb722490e1f300b80aaec
nuttapong_zack <nuttapong.spn15@gmail.com>
2025-10-05 16:17:32 +0700

    create varity calculate app by next.js

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>ไม่พบหน้าที่ต้องการ กรุณาตรวจสอบ URL หรือลองใหม่</h1>
+    </>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-lg text-gray-800">
+          Created by ZacK DTI-SAU
+        </h1>
+        <h1 className="text-center text-lg text-gray-800">
+          Copyright &copy; 2025
+        </h1>
+      </div>
+    </>
+  );
+}
